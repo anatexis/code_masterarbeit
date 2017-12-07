0ad70fac1a6ad688df980090bcc3459fbcccf3d3
anatexis <ichselberan-github@yahoo.de>
2017-12-07 16:57:14 +0100

    interpol funkt nicht wirklich

--- timeseries.R
+++ timeseries.R
@@ -69,3 +69,51 @@ plot_wt<- ggplot(wt,aes(x =X1 , y = X3)) +
         geom_point(stat="identity") 
 plot_wt
 
+
+#################################################### 2 jhre
+
+setwd("/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/Stationsdaten")
+file <- "LT107300.dat"
+
+at <- read_table(file, col_names = F, skip = 20, cols(
+        X1 = col_date(format = "%d.%m.%Y"),
+        X2 = col_time(format = ""),
+        X3 = col_double()
+))
+
+
+#subsetting
+at2 <- at[as_date(at$X1)<as_date("2015-01-01"), ]
+at2 <- at2[as_date(at2$X1)>as_date("2013-01-01"), ]
+# plot
+
+plot_at<- ggplot(at2,aes(x =X1 , y = X3)) + 
+        xlab("time") + ylab("air temperature") +
+        expand_limits(y=c(-10,30))+
+        ggtitle("air temperature curve")+
+        geom_point(stat="identity") 
+plot_at
+
+####
+
+setwd("/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/Stationsdaten")
+file <- "WT-Tagesmitte-Loich.dat"
+
+wt <- read_table(file, col_names = F, skip = 26, cols(
+        X1 = col_date(format = "%d.%m.%Y"),
+        X2 = col_time(format = ""),
+        X3 = col_double()
+))
+
+
+#subsetting
+wt <- wt[as_date(wt$X1)<as_date("2015-01-01"), ]
+# plot
+
+plot_wt<- ggplot(wt,aes(x =X1 , y = X3)) + 
+        xlab("time") + ylab("water temperature") +
+        expand_limits(y=c(-10,30))+
+        ggtitle("water temperature curve")+
+        geom_point(stat="identity") 
+plot_wt
+
