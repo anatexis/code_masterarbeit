9bc214fc794642dc2a8deafdebc5eae04ec5a477
anatexis <ichselberan-github@yahoo.de>
2018-01-06 16:38:53 +0100

    rounding added firt try to add PET, didn't work

--- merge_temp_precipitation.R
+++ merge_temp_precipitation.R
@@ -25,15 +25,28 @@ pst_T <- read_table(file2, col_names = F, cols( X1 = col_date(format = "%d%m%Y")
 pst_P1 <- pst_P[as_date(pst_P$X1) > as_date("1990-12-31"), ]
 pst_T1 <- pst_T[as_date(pst_T$X1) < as_date("2015-01-01"), ]
 
-# merge datasets
+# merge T and P and PET
 P_T <- add_column(pst_T1,pst_P1$X2)
 
-P_T$X1 <- format(P_T$X1, "%d%m%Y") # für input modna
+P_T$X1 <- format(P_T$X1, "%d%m%Y") # for input modna
+
+#add pot ET (calculated)
+setwd ("/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/")
+file3 <- "petout.txt"
+PET <- read_table(file3, col_names = F, cols(X1 = col_date(format = "%d%m%Y"),
+                                                 X2 = col_double()
+))
+
+P_T <- add_column(PET$X2) #doesnt work because PET has missing values (sometimes no 01.01., year starts with 02.01??)
 
 
+# rounding to one digit
 
-setwd ("/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/EZG/output//")
-# comment not to overwrite stuff
-# write.table(P_T,file = paste(format(Sys.time(), "%Y-%m-%d"),
-#                                       "_tnse", ".txt", sep = "") ,sep=" ", row.names=FALSE,
-#             col.names = c("Datum", "t", "NSeff"), quote = F)
+P_T$X2 <- round(P_T$X2,1)
+P_T$`pst_P1$X2` <- round(P_T$`pst_P1$X2`,1)
+
+setwd ("/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/EZG/output/")
+# commented to not overwrite stuff
+ write.table(P_T,file = paste(format(Sys.time(), "%Y-%m-%d"),
+                                       "_tnse", ".txt", sep = "") ,sep=",", row.names=FALSE,
+             col.names = c("Datum", "t", "NSeff"), quote = F)
