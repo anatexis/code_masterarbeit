0b97d70b9cc67bc3f75e2845caedfb35b4043bbd
anatexis <ichselberan-github@yahoo.de>
2018-01-11 20:50:26 +0100

    changed date formate and q from m3/s to mm

--- q_obs.R
+++ q_obs.R
@@ -13,16 +13,18 @@ rff <- read_table(file, col_names = F, skip = 26, cols(
 #subsetting to epot beobachtungszeitraum bis 2014-01-16
 q_obs <- rff[as_date(rff$X1)<as_date("2014-01-17"), ]
 
+# m³/s to mm/d 
+q_obs <- mutate(q_obs,
+       q_mm=round(X3*1000*60*60*24/142494769,2))
+
 # selecting only date and q
 # select doesn't work when raster is loaded
 if (isNamespaceLoaded("raster") == T) detach("package:raster", unload=TRUE)
-q_obs <- select(rff, X1, X3)
+q_obs <- select(q_obs, X1, q_mm)
 
-# m³/s to mm/d 
-q_obs <- mutate(q_obs,
-       X3=X3*1000*60*60*24/142494769)
+## change formate for fortran input
+q_obs$X1 <- format(q_obs$X1, "%d%m%Y") 
 
-## to do datum anpassen
 #write
 setwd("/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/output_R")
 
