619d8f15b93d79c6eb8dec6ead89c88283bb867a
anatexis <ichselberan-github@yahoo.de>
2018-04-09 08:12:50 +0200

    just changing the parameters

--- graph_modna-output.R
+++ graph_modna-output.R
@@ -30,25 +30,53 @@ Q <- ggplot(data= discharge)+
   geom_line( aes(x=TTMMYYYY, y=linout, color = "lin"))+
   geom_line( aes(x=TTMMYYYY, y=cascout, color = "casc"))+
   xlab("Date")+
-  ylab("Discharge")+
+  ylab("Discharge [mm]")+
   annotate("text", x=as.Date(16022), y=37,label="nse= ")+
-  annotate("text", x=as.Date(16060), y=37,label=as.character(round(nse,2)))+
+  annotate("text", x=as.Date(16064), y=37,label=as.character(round(nse,2)))+
   annotate("text", x=as.Date(16022), y=34,label="kge= ")+
   annotate("text", x=as.Date(16064), y=34,label=as.character(round(kge,2)))
   
 Q
-
-setwd("C:/Users/Katrin/Desktop/master/plotfiles")
+setwd("C:/Users/Katrin/Desktop/master/plotfiles_neu")
 file = paste(format(Sys.time(), "%Y-%m-%d_%H-%M"),"_Q",".png",sep="")
+ggsave(file)
+
+Q_end <- ggplot(data= discharge)+
+  geom_line( aes(x=TTMMYYYY, y=qsim, color = ".Qsim"))+
+  geom_line( aes(x=TTMMYYYY, y=Qobs, color = "Qobs"))+
+  xlab("Date")+
+  ylab("Discharge [mm]")+
+  annotate("text", x=as.Date(16022), y=37,label="nse= ")+
+  annotate("text", x=as.Date(16064), y=37,label=as.character(round(nse,2)))+
+  annotate("text", x=as.Date(16022), y=34,label="kge= ")+
+  annotate("text", x=as.Date(16064), y=34,label=as.character(round(kge,2)))
+
+Q_end
+
+setwd("C:/Users/Katrin/Desktop/master/plotfiles_nur_Q_END")
+file = paste(format(Sys.time(), "%Y-%m-%d_%H-%M"),"_Q_END",".png",sep="")
 ggsave(file)
 
 ### damit ich nachvollziehen kann was ich gemacht hab
 casc <- "1"
-k <- "5"
-split <- "0.35"
-rootstor <- "60"
-changes <- tibble(casc,k,split,rootstor)
+kcasc <- "2"
+klin <- "50"
+split <- "0.45"
+rootstor <- "70"
+changes <- tibble(casc,kcasc,klin,split,rootstor)
 write.table(changes,file = paste(format(Sys.time(), "%Y-%m-%d_%H-%M"),
                                 "_Q", ".txt", sep = "") ,sep=",",
-            row.names=FALSE,col.names = c("casc", "k", "split", "rootst"),
+            row.names=FALSE,col.names = c("casc", "kcask", "klin", "split", "rootstor"),
             eol = "\r\n", quote = F)
+
+# auch in plotfiles_neu schreiben
+setwd("C:/Users/Katrin/Desktop/master/plotfiles_neu")
+changes <- tibble(casc,kcasc,klin,split,rootstor)
+write.table(changes,file = paste(format(Sys.time(), "%Y-%m-%d_%H-%M"),
+                                 "_Q", ".txt", sep = "") ,sep=",",
+            row.names=FALSE,col.names = c("casc", "kcask", "klin", "split", "rootstor"),
+            eol = "\r\n", quote = F)
+
+#that I can compare nse kge better
+nse
+kge
