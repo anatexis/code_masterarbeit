880861590712e982ce0fc187ca7f7213cb3dd4f1
anatexis win7 <ichselberan-github@yahoo.de>
2018-04-16 18:08:20 +0200

    comments in english, fixed colors, sources

--- graph_modna-output.R
+++ graph_modna-output.R
@@ -34,7 +34,9 @@ Q <- ggplot(data= discharge)+
   annotate("text", x=as.Date(16022), y=37,label="nse= ")+
   annotate("text", x=as.Date(16064), y=37,label=as.character(round(nse,2)))+
   annotate("text", x=as.Date(16022), y=34,label="kge= ")+
-  annotate("text", x=as.Date(16064), y=34,label=as.character(round(kge,2)))
+  annotate("text", x=as.Date(16064), y=34,label=as.character(round(kge,2)))+
+  scale_color_manual(values=c("Qobs"="#00BFC4", "Qsim"="#C77CFF",
+                              "lin"="#7CAE00", "casc"="#F8766D"))
   
 Q
 setwd("C:/Users/Russ/Desktop/master/plotfiles_neu")
@@ -49,7 +51,8 @@ Q_end <- ggplot(data= discharge)+
   annotate("text", x=as.Date(16022), y=37,label="nse= ")+
   annotate("text", x=as.Date(16064), y=37,label=as.character(round(nse,2)))+
   annotate("text", x=as.Date(16022), y=34,label="kge= ")+
-  annotate("text", x=as.Date(16064), y=34,label=as.character(round(kge,2)))
+  annotate("text", x=as.Date(16064), y=34,label=as.character(round(kge,2)))+
+  scale_color_manual(values=c("Qobs"="#00BFC4", ".Qsim"="#F8766D"))
 
 Q_end
 
@@ -57,16 +60,15 @@ setwd("C:/Users/Russ/Desktop/master/plotfiles_nur_Q_END")
 file = paste(format(Sys.time(), "%Y-%m-%d_%H-%M"),"_Q_END",".png",sep="")
 ggsave(file, height = 3.368173, width = 4.27, units = "in")
 
-### damit ich nachvollziehen kann was ich gemacht hab wird inputmodna verschoben
+##to trace my changes copy inputfile.txt to directories of plots
 
-#(quelle: https://stackoverflow.com/questions/2384517/using-r-to-copy-files#2384621)
 file <- list.files("C:/Users/Russ/Desktop/master/daten/input/",
                     "inputmodna.txt", full.names = TRUE)
 file.copy(file,"C:/Users/Russ/Desktop/master/plotfiles_neu")
 file.copy(file,"C:/Users/Russ/Desktop/master/plotfiles_nur_Q_END/")
 
-#umbenennen
-#Quelle: https://stackoverflow.com/questions/10758965/how-do-i-rename-files-using-r
+##rename files
+
 # in plotfiles_nur_Q_END
 file.rename("inputmodna.txt",paste(format(Sys.time(), "%Y-%m-%d_%H-%M"),
                                    "_inputmodna", ".txt", sep = ""))
@@ -77,6 +79,14 @@ setwd("C:/Users/Russ/Desktop/master/plotfiles_neu")
 file.rename("inputmodna.txt",paste(format(Sys.time(), "%Y-%m-%d_%H-%M"),
                                    "_inputmodna", ".txt", sep = ""))
 
+
+#sources:
+
+#copy files: https://stackoverflow.com/questions/2384517/using-r-to-copy-files#2384621
+#rename files: https://stackoverflow.com/questions/10758965/how-do-i-rename-files-using-r
+#scale_color_manual:https://stackoverflow.com/a/40181166
+
+#------
 #that I can compare nse kge better
 nse
 kge
