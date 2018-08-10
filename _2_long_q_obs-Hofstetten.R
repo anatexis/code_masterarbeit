966b775a9a7df101d47969fb568f3614bda3da8c
anatexis win7 <ichselberan-github@yahoo.de>
2018-08-10 15:54:10 +0200

    output file for q_obs created

--- _2_long_q_obs-Hofstetten.R
+++ _2_long_q_obs-Hofstetten.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(lubridate)
 ################### runoff
+
+# WAS FEHLT: SACHEN RAUS, DIE NUR für LOICH VON BEDEUTUNG SIND
+
 path <- "/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/Stationsdaten"
 if( .Platform$OS.type == "windows" )
   path <- "C:/Users/Russ/Desktop/master/daten/Stationsdaten/"
@@ -16,23 +19,26 @@ rff <- read_table(file, col_names = F, skip = 27, na = "Lücke", cols(
 
 sum(is.na(rff[3])) # just 
 
-#subsetting to epot beobachtungszeitraum bis 2014-01-16
-q_obs <- rff[as_date(rff$X1)<as_date("2014-01-17"), ]
+# #subsetting to epot beobachtungszeitraum bis 2014-01-16 [[loich!]]
+# q_obs <- rff[as_date(rff$X1)<as_date("2014-01-17"), ]
 
 
 # selecting only date and q
 # select doesn't work when raster is loaded
 if (isNamespaceLoaded("raster") == T) detach("package:raster", unload=TRUE)
-q_obs <- select(q_obs, X1, X3)
+q_obs <- select(rff, X1, X3)
 
 ## change formate for fortran input
 q_obs$X1 <- format(q_obs$X1, "%d%m%Y") 
 
 #write
-setwd("/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/output_R")
+path <- "/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/output_R"
+if( .Platform$OS.type == "windows" )
+  path <- "C:/Users/Russ/Desktop/master/daten/output_R//"
+setwd(path)
 
 write.table(q_obs,file = paste(format(Sys.time(), "%Y-%m-%d"),
-                             "_q_obs", ".txt", sep = "") ,sep=",",
+                             "_Hofstetten_q_obs", ".txt", sep = "") ,sep=",",
             row.names=FALSE, col.names = c("Datum", "Q"),
             eol = "\r\n", quote = F)
 
