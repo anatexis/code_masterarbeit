faa814cfffea57729c8126e64b1f03b577417dd9
anatexis win7 <ichselberan-github@yahoo.de>
2018-08-12 14:10:27 +0200

    korrigierten daten (von etpot_main) eingelesen, output geschrieben

--- _3z_long_merge_P_plus_T_plus_epot_Hofstetten.R
+++ _3z_long_merge_P_plus_T_plus_epot_Hofstetten.R
@@ -29,10 +29,10 @@ pst_T <- read_table(file2, col_names = F, cols( X1 = col_date(format = "%d%m%Y")
 # cut timeseries to overlapping period from 01.01.1991 to 16.01.2014 
 
 pst_P1 <- pst_P[as_date(pst_P$X1) > as_date("1990-12-31"), ]
-pst_P1 <- pst_P1[as_date(pst_P1$X1) < as_date("2016-01-01"), ]
+pst_P1 <- pst_P1[as_date(pst_P1$X1) < as_date("2015-01-01"), ]
 
 pst_T <- pst_T[as_date(pst_T$X1) > as_date("1990-12-31"), ]
-pst_T1 <- pst_T[as_date(pst_T$X1) < as_date("2016-01-01"), ]
+pst_T1 <- pst_T[as_date(pst_T$X1) < as_date("2015-01-01"), ]
 
 # merge T and P
 P_T <- add_column(pst_T1,pst_P1$X2)
@@ -41,43 +41,56 @@ P_T <- add_column(pst_T1,pst_P1$X2)
 P_T$X2 <- round(P_T$X2,1)
 P_T$`pst_P1$X2` <- round(P_T$`pst_P1$X2`,1)
 
+#removing all 29.02 from dataframw
+P_T2902rm <- P_T[as.numeric(strftime(P_T$X1, "%m%d")) != 229,]
+
+# add pot ET (calculated from fortran epot_main.for) 
+### achtung bugs! holzmann noch schreiben
+
+path <- "/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/output_etpot_main/"
+if( .Platform$OS.type == "windows" )
+  path <- "C:/Users/Russ/Desktop/master/daten/output_etpot_main/"
+setwd(path)
+
+file3 <- "petout_hofstn_korr_korr_libreoffice_export.csv"
+
+library(stringi)
+### to get r to read in files with in the form of
+### dmmyyy AND ddmmyyy we have to do smt like this:
+PET <- read_table(file3, col_names = c("X1","X2"),col_types = c("c","d")) #schreit fehler aber passt!??
+
+stri_sub(PET$X1,-6,0) <- "-"
+stri_sub(PET$X1,-4,0) <- "-"
+PET$X1 <- as.Date(PET$X1, "%d-%m-%Y")
 
 
-##########################################################################################################
-##########################################################################################################
-###      TO DO IN FORTRAN ACHTUNG AUF FEHLER! (siehe bugs in ordner von win7 laptop) #####################
-#########################################################################################################
-# add pot ET (calculated from fortran epot_main.for)
-setwd ("/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/")
-file3 <- "petout_richtig3.txt"
-PET <- read_table(file3, col_names = F, cols(X1 = col_date(format = "%d%m%Y"),
-                                                 X2 = col_double()
-))
-
-PET <- PET[as_date(PET$X1) > as_date("2012-12-31"), ]
-PET <- PET[as_date(PET$X1) < as_date("2014-01-01"), ]
+PET <- PET[as_date(PET$X1) > as_date("1990-12-31"), ]
+PET <- PET[as_date(PET$X1) < as_date("2015-01-01"), ]
 
 # P_T_ET variable with precipitation, temp and Evapotranspiration
-P_T_ET <- add_column(P_T,PET$X2)
+P_T_ET <- add_column(P_T2902rm,PET$X2)
 
-# start timeseries at 20.11.2012 because thats earliest q observation
-P_T_ET <- P_T_ET[as_date(P_T_ET$X1) > as_date("2012-12-31"), ]
+# # start timeseries at 20.11.2012 because thats earliest q observation
+# P_T_ET <- P_T_ET[as_date(P_T_ET$X1) > as_date("2012-12-31"), ]
 
 # change date format for input modna
 P_T_ET$X1 <- format(P_T_ET$X1, "%d%m%Y") 
 
+# #schon davor passiert
+# #removing all 29.02 from dataframe and change formate (there is no 29.02 in ts 
+# # from 20.11.2012-17.01.2014 but maybe we get longer data for q)
+# P_T2902rm <- P_T_ET[as.numeric(strftime(P_T$X1, "%m%d")) != 229,]
 
-#removing all 29.02 from dataframe and change formate (there is no 29.02 in ts 
-# from 20.11.2012-17.01.2014 but maybe we get longer data for q)
-P_T2902rm <- P_T_ET[as.numeric(strftime(P_T$X1, "%m%d")) != 229,]
 
+path <- "/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/output_R/"
+if( .Platform$OS.type == "windows" )
+  path <- "C:/Users/Russ/Desktop/master/daten/output_R/"
+setwd(path)
 
-
-setwd ("/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/output_R/")
 
 # commented out when written
 # complete data
 write.table(P_T_ET,file = paste(format(Sys.time(), "%Y-%m-%d"),
-                                "P_T_ET-2013", ".txt", sep = "") ,sep=",",
+                                "P_T_ET-Hofstetten", ".txt", sep = "") ,sep=",",
             row.names=FALSE,col.names = c("Datum", "t", "NSeff", "ET"),
             eol = "\r\n", quote = F)
