f70b75156612b00f13ca59a4b6b495a440e15257
anatexis win7 <ichselberan-github@yahoo.de>
2018-08-26 14:09:11 +0200

    there is a trend but just a very small one. can I ignore it? I will find out

--- _3y_long_is-there-a-trend.R
+++ _3y_long_is-there-a-trend.R
@@ -24,8 +24,24 @@ PTET <- read_csv(file1, col_names = T, cols(Datum = col_date(format = "%d%m%Y"),
 
 Qobs <- read_csv(file2, col_names = T, cols( Datum = col_date(format = "%d%m%Y"),
                                                 Q = col_double()))
+################### for R² on plot#####################
+
+lm_eqn <- function(df,i){
+  date0 <- df[[1]]-1990
+  m <- lm(df[[i]] ~ date0, df);
+  eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
+                   list(a = format(coef(m)[1], digits = 2), 
+                        b = format(coef(m)[2], digits = 2), 
+                        r2 = format(summary(m)$r.squared, digits = 3)))
+  as.character(as.expression(eq));                 
+}
+# source:https://stackoverflow.com/a/7549819
+
+###########################################################
 
 
+
+#################### compare mean of years #################
 ### yearly Qobs
 # dplyr and pipe ftw!
 yearly_Qobs <- Qobs %>%
@@ -40,7 +56,8 @@ P_Qobs <- ggplot(data= yearly_Qobs,aes(x=year, y=Qobs))+
   #  geom_line( aes(x=month, y=linout, color = "lin"))+
   #  geom_line( aes(x=month, y=cascout, color = "casc"))+
   xlab("Date")+
-  ylab("mean yearly discharge [m³/s]")
+  ylab("mean yearly discharge [m³/s]") +
+  geom_text(x = 2005, y = 8.5 ,label = lm_eqn(yearly_Qobs,2), parse = TRUE) # from above 
 P_Qobs
 
 
@@ -55,13 +72,14 @@ yearly_PTET <- PTET %>%
     )
 
 #temp
-P_t <- ggplot(data = yearly_PTET,aes(x=year, y=t))+
+P_t <- ggplot(data = yearly_PTET,aes(x = year, y=t))+
   geom_line()+
   geom_smooth(method="lm")+
   #  geom_line( aes(x=month, y=linout, color = "lin"))+
   #  geom_line( aes(x=month, y=cascout, color = "casc"))+
   xlab("Date")+
-  ylab("mean yearly Temperature")
+  ylab("mean yearly Temperature") +
+  geom_text(x = 2005, y = 7.5 ,label = lm_eqn(yearly_PTET,2), parse = TRUE) # from above 
 P_t
 
 #NSeff
@@ -71,7 +89,8 @@ P_NSeff <- ggplot(data = yearly_PTET,aes(x=year, y=NSeff))+
   #  geom_line( aes(x=month, y=linout, color = "lin"))+
   #  geom_line( aes(x=month, y=cascout, color = "casc"))+
   xlab("Date")+
-  ylab("mean yearly NSeff")
+  ylab("mean yearly NSeff") +
+  geom_text(x = 1998, y = 4.5 ,label = lm_eqn(yearly_PTET,3), parse = TRUE) # from above 
 P_NSeff
 
 P_ET <- ggplot(data = yearly_PTET,aes(x=year, y=ET))+
@@ -80,9 +99,52 @@ P_ET <- ggplot(data = yearly_PTET,aes(x=year, y=ET))+
   #  geom_line( aes(x=month, y=linout, color = "lin"))+
   #  geom_line( aes(x=month, y=cascout, color = "casc"))+
   xlab("Date")+
-  ylab("mean yearly ET")
+  ylab("mean yearly ET") +
+  geom_text(x = 2005, y = 1.55 ,label = lm_eqn(yearly_PTET,4), parse = TRUE)
 P_ET
+#
+#
+#
+#
+#
+#
+################ compare mean of months of each year #############
+### monthly mean per year Qobs
+# dplyr and pipe ftw!
+monthly_Qobs <- Qobs %>%
+  group_by(year = month(Datum))%>%
+  summarise(
+    Qobs = mean(Q))
 
+
+P_Qobs <- ggplot(data= monthly_Qobs,aes(x=year, y=Qobs))+
+  geom_line()+
+  geom_smooth(method="lm")+
+  #  geom_line( aes(x=month, y=linout, color = "lin"))+
+  #  geom_line( aes(x=month, y=cascout, color = "casc"))+
+  xlab("Date")+
+  ylab("mean yearly discharge [m³/s]")
+P_Qobs
+
+
+# yearly PTET-stuff
+monthly_PTET <- PTET %>%
+  group_by(year = year(Datum), month = month(Datum))%>%
+  summarise(
+    t = mean(t),
+    NSeff = mean(NSeff),
+    ET = mean(ET)
+  )
+
+#temp
+P_tm <- ggplot(data = monthly_PTET,aes(x = month, y=t))+
+  geom_point()+
+  geom_smooth(method="lm")+
+  #  geom_line( aes(x=month, y=linout, color = "lin"))+
+  #  geom_line( aes(x=month, y=cascout, color = "casc"))+
+  xlab("Date")+
+  ylab("mean monthly Temperature each year")
+P_tm
 
 # 
 # # cut timeseries to overlapping period from 01.01.1991 to 16.01.2014 
