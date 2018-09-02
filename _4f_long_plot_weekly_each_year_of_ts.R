e744c40b0876936bfbc0a0cf442fc4a3804ca0e3
anatexis win7 <ichselberan-github@yahoo.de>
2018-09-02 21:18:58 +0200

    _4f_ works, I put the grouping in the loop

--- _4f_long_plot_weekly_each_year_of_ts.R
+++ _4f_long_plot_weekly_each_year_of_ts.R
@@ -1,3 +1,4 @@
+# final calibration of daily values aggregated to weekly values
 #install.packages("hydroTSM")
 library(tidyverse)
 library(lubridate)
@@ -18,15 +19,13 @@ discharge <- head(discharge, -1) # delete last row because its a duplicat (fault
 tail(discharge)
 # now the dates are correctly read in
 
-#calculate qsim and select output which is interesting for us
-discharge <- discharge %>% mutate(qsim=linout + cascout) %>% 
-  select(., TTMMYYYY,Qobs,qsim,linout,cascout)
+
 library(hydroGOF)
 
-setwd("C:/Users/Russ/Desktop/master/plotfiles_Hofstetten/plot_each_year/")
+setwd("C:/Users/Russ/Desktop/master/plotfiles_Hofstetten/plot_weekly_each_year/")
 
 
-
+# group_by was put in the loop
 for ( i in seq_len(2002-(1991)+2)){
   
   #prepare vectors for loop
@@ -34,16 +33,26 @@ for ( i in seq_len(2002-(1991)+2)){
   x <- c(0,365*y) 
   discharge_plot <- discharge[(x[i]+1):x[i+1],]
   
-  nse <- NSE(discharge_plot$qsim,discharge_plot$Qobs)
-  kge <- KGE(discharge_plot$qsim,discharge_plot$Qobs)
+  discharge_plot_w <- discharge_plot %>% 
+    mutate(qsim=linout + cascout) %>% 
+    select(., TTMMYYYY,Qobs,qsim,linout,cascout) %>%
+    group_by(week = week(TTMMYYYY))%>% ### calculate weekly discharge
+    summarise(
+      Qobs = mean(Qobs),
+      qsim = mean(qsim),
+      linout = mean(linout),
+      cascout = mean(cascout))
+  
+  nse <- NSE(discharge_plot_w$qsim,discharge_plot_w$Qobs)
+  kge <- KGE(discharge_plot_w$qsim,discharge_plot_w$Qobs)
   
   year <- 1990 + i
   
-  Q <- ggplot(data= discharge_plot)+
-    geom_line( aes(x=TTMMYYYY, y=Qobs, color = "Qobs"))+
-    geom_line( aes(x=TTMMYYYY, y=qsim, color = "Qsim"))+
-    geom_line( aes(x=TTMMYYYY, y=linout, color = "lin"))+
-    geom_line( aes(x=TTMMYYYY, y=cascout, color = "casc"))+
+  Q <- ggplot(data= discharge_plot_w)+
+    geom_line( aes(x=week, y=Qobs, color = "Qobs"))+
+    geom_line( aes(x=week, y=qsim, color = "Qsim"))+
+    geom_line( aes(x=week, y=linout, color = "lin"))+
+    geom_line( aes(x=week, y=cascout, color = "casc"))+
     ggtitle(paste("In the Year", year, "NSE is ",round(nse,2))) +
     xlab("Date")+
     ylab("Discharge [mm]")+
@@ -55,7 +64,3 @@ for ( i in seq_len(2002-(1991)+2)){
   
   
 }
-
-
-(nse <- NSE(discharge$qsim,discharge$Qobs))
-(kge <- KGE(discharge$qsim,discharge$Qobs))
