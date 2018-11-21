a6a745343593b32aa692afda43359dbb927df94e
anatexis win7 <ichselberan-github@yahoo.de>
2018-11-21 11:39:28 +0100

    _7b_ changed order of WT (above GWT) and calulated different lags

--- _7b_FKA_6f_read_in_Q_WT_AT_GWT.R
+++ _7b_FKA_6f_read_in_Q_WT_AT_GWT.R
@@ -132,71 +132,6 @@ AT_m_v <- airtemp_v %>% select(., date=TTMMYYY,AirTemp=Temp)%>%
 
 AT_m <- rbind(AT_m_c, AT_m_v) # now we have the air temperature data from 1991 to 2014
 
-
-################################################################################################
-###################                  Einlesen der GWT-Daten            #########################
-################################################################################################
-
-path <- "/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/Stationsdaten"
-if( .Platform$OS.type == "windows" )
-  path <- "C:/Users/Russ/Desktop/master/daten/Stationsdaten/"
-setwd(path)
-
-file1 <- "Grundwassertemperatur-Monatsmittel-322917.csv" #1.näheste Station
-
-
-gwst1 <- read_csv2(file1, col_names = F, skip = 34, na = "Lücke",
-                        cols(
-                          X1 = col_date(format = "%d.%m.%Y %T"), 
-                          X2 = col_double())) %>% select(.,date=X1,GWTemp=X2)
-
-
-
-# subset to 1991-2014
-gwst1
-gwtemp_m <- gwst1[as_date(gwst1$date) < as_date("2015-01-01"), ]
-GWT_m <- gwtemp_m[as_date(gwtemp_m$date) > as_date("1990-12-31"), ]
-tail(gwtemp_m)
-# jahre in denen NAs vorkommen:
-
-GWT_m[is.na(GWT_m$GWTemp),]  # 2016 nicht mehr in subsetting bereich
-
-gwtemp_m[is.na(gwtemp_m$GWTemp),]
-plot(gwtemp_m,type="l")
-
-#######################
-GWT_m # from 1991 to 2014 without lag
-######################
-
-
-# ###preparatioin for lag (we have to include 3 more months because we will lose three bc of lag -3)
-# 
-# 
-# path <- "/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/Stationsdaten"
-# if( .Platform$OS.type == "windows" )
-#   path <- "C:/Users/Russ/Desktop/master/daten/Stationsdaten/"
-# setwd(path)
-# 
-# file1 <- "Grundwassertemperatur-Monatsmittel-322917.csv"
-# 
-# gwst1 <- read_csv2(file1, col_names = F, skip = 34, na = "Lücke",cols(
-#   X1 = col_date(format = "%d.%m.%Y %T"), 
-#   X2 = col_double()
-# )) %>% select(.,date=X1,GWTemp=X2)
-# 
-# #subset to 1991-2014 + 3 months
-# gwst1
-# gwtemp_m <- gwst1[as_date(gwst1$date) < as_date("2015-04-01"), ]
-# GWT_m2 <- gwtemp_m[as_date(gwtemp_m$date) > as_date("1990-12-31"), ]
-# 
-# GWT_m_lag <- GWT_m2
-# GWT_m_lag$GWTemp <- lead(GWT_m2$GWTemp, 3) #lead 3
-# GWT_m_lag <- GWT_m_lag[1:288,] # to remove the last three NAs
-
-
-# end of preparation ########################
-
-
 ################################################################################################
 ###################                  Einlesen der WT-Daten             #########################
 ################################################################################################
@@ -233,4 +168,104 @@ WT_m_NA <- WT_m[is.na(WT_m$WTemp),] # von 2014-6 bis 2016-10 (29 Datenpkt) fehle
 #View(WT_m_NA)  
 
 plot(WT_m$WTemp, type="l")
+
+################################################################################################
+###################                  Einlesen der GWT-Daten            #########################
+################################################################################################
+
+path <- "/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/Stationsdaten"
+if( .Platform$OS.type == "windows" )
+  path <- "C:/Users/Russ/Desktop/master/daten/Stationsdaten/"
+setwd(path)
+
+file1 <- "Grundwassertemperatur-Monatsmittel-322917.csv" #1.näheste Station
+
+
+gwst1 <- read_csv2(file1, col_names = F, skip = 34, na = "Lücke",
+                        cols(
+                          X1 = col_date(format = "%d.%m.%Y %T"), 
+                          X2 = col_double())) %>% select(.,date=X1,GWTemp=X2)
+
+
+
+# subset to 1991-2014
+gwst1
+gwtemp_m <- gwst1[as_date(gwst1$date) < as_date("2015-01-01"), ]
+GWT_m <- gwtemp_m[as_date(gwtemp_m$date) > as_date("1990-12-31"), ]
+tail(gwtemp_m)
+# jahre in denen NAs vorkommen:
+
+GWT_m[is.na(GWT_m$GWTemp),]  # 2016 nicht mehr in subsetting bereich
+
+gwtemp_m[is.na(gwtemp_m$GWTemp),]
+plot(gwtemp_m,type="l")
+
+#######################
+GWT_m # from 1991 to 2014 without lag
+
+
+############################# lags for 7c our model    #######################################
+#function ein bisschen anders, gibt meherere aus wenn sie ähnlich groß sind
+
+Find_Max_CCF <- function(a, b, e=0)
+{
+  d <- ccf(a, b, plot = FALSE, lag.max = length(a)/2, na.action = na.pass)
+  cor = d$acf[,,1]
+  abscor = abs(d$acf[,,1])
+  lag = d$lag[,,1]
+  res = data.frame(cor, lag)
+  absres = data.frame(abscor, lag)
+  maxcor = max(absres$abscor)
+  absres_max = res[which(absres$abscor >= maxcor-maxcor*e &
+                           absres$abscor <= maxcor+maxcor*e),]
+  return(absres_max)
+}
+
+# Find_Max_CCF(GWT_m$GWTemp[1:161],WT_m$WTemp[1:161], 0.02) #(calibration period)
+# cor       lag
+# 0.8532170   3*
+# 0.8502565   4
+## lag for the validaton period (which is from month 191:288)
+
+Find_Max_CCF(GWT_m$GWTemp[191:288],WT_m$WTemp[191:288], 0.02)
+# cor       lag
+# 0.8820232   3
+# 0.8931690   4*
+
+## lag for whole period
+
+Find_Max_CCF(GWT_m$GWTemp,WT_m$WTemp,0.02)
+# cor         lag
+# 0.8522380   3
+# 0.8564982   4*
+
+###preparatioin for lag in _7c_
+# for the we have to include 4 more months because we will lose three bc of lag 4)
+
+
+path <- "/home/christoph/Dokumente/BOKU/Masterarbeit/Daten/Stationsdaten"
+if( .Platform$OS.type == "windows" )
+  path <- "C:/Users/Russ/Desktop/master/daten/Stationsdaten/"
+setwd(path)
+
+file1 <- "Grundwassertemperatur-Monatsmittel-322917.csv"
+
+gwst1 <- read_csv2(file1, col_names = F, skip = 34, na = "Lücke",cols(
+  X1 = col_date(format = "%d.%m.%Y %T"),
+  X2 = col_double()
+)) %>% select(.,date=X1,GWTemp=X2)
+
+#subset to 1991-2014 + 3 months
+gwst1
+gwtemp_m <- gwst1[as_date(gwst1$date) < as_date("2015-05-01"), ]
+GWT_m2 <- gwtemp_m[as_date(gwtemp_m$date) > as_date("1990-12-31"), ]
+
+GWT_m_lag <- GWT_m2
+GWT_m_lag$GWTemp <- lead(GWT_m2$GWTemp, 4) #lead 3
+GWT_m_lag <- GWT_m_lag[1:288,] # to remove the last three NAs
+
+# end of preparation ########################
+
+
+
 
