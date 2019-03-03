880ffcadb991f25277778dc0c2ad45c7aa38e38d
anatexis win7 <ichselberan-github@yahoo.de>
2019-03-03 11:56:24 +0100

    _8a_ plot of fast-slow components of discharge

---
+++ _8a_plots_for_thesis.R
@@ -0,0 +1,30 @@
+
+
+######### fast&slow runoff! validation (for hydrological model!) period
+Q_m_v34 <- Q_m_v[4:5] %>% # I have tu subset the tibble like this, with select(.,fast,slow) it doesnt work
+  rowid_to_column(.,"rowid") %>% 
+  gather(.,Q_type,Q,-rowid)
+
+(p <- ggplot(Q_m_v34, aes(rowid, Q, fill = Q_type)) +
+    xlab("time [Months]") + ylab("Runoff [mm]") +
+    ggtitle("Runoff components (validation period)")+
+    geom_bar( stat = "identity")
+)
+
+########## fast&slow runoff one year
+Q_m_v34_year2005 <- Q_m_v[13:24,4:5] %>% 
+  rowid_to_column(.,"rowid") %>% 
+  gather(.,Q_type,Q,-rowid)
+
+(p <- ggplot(Q_m_v34_year2005, aes(rowid, Q, fill = Q_type)) +
+    xlab("Months") + ylab("Runoff [mm]") +
+    ggtitle("Runoff components (year 2005)")+
+    geom_bar( stat = "identity")+
+    scale_x_continuous(breaks=1:12)
+)
+
+###sources:
+#https://stackoverflow.com/a/6693427 (put in data long format)
+#https://stackoverflow.com/questions/43228109/problems-converting-from-wide-to-long-using-reshape-tibble-issue#comment73526615_43228109 (use gather rather than melt)
+# ?gather (how to use it)
+       
